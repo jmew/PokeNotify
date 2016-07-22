c4d64d1435607876555808851195a761ed025f4d
Jeffrey Mew <thehonourablejeffrey@gmail.com>
2016-07-21 22:50:52 -0400

    Initial commit

---
+++ app/src/main/java/com/jeffreymew/pokenotify/MapFragment.java
@@ -0,0 +1,20 @@
+package com.jeffreymew.pokenotify;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Mew on 2016-07-21.
+ */
+public class MapFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
